8c242e9cdf7812ac44f5f614dfdc14e97f738f0e
Leonid Shamis <leonid.shamis@gmail.com>
2021-11-16 13:42:48 -0800

    str

---
+++ str.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <sstream>
+#include <string>
+#include <vector>
+
+namespace util {
+
+template <typename... Arg>
+std::string cat(Arg&&... arg) {
+  std::ostringstream ss;
+  (void)(ss << ... << std::forward<Arg>(arg));
+  return ss.str();
+}
+
+template <typename Container>
+std::string join(Container c) {
+  std::ostringstream ss;
+  for (auto&& v : c) {
+    ss << v;
+  }
+  return ss.str();
+}
+
+template <typename... Args>
+std::string fmt(std::string_view format, Args... args) {
+  size_t size = snprintf(nullptr, 0, format.data(), args...);
+  std::vector<char> buf(size + 1);
+  sprintf(buf.data(), format.data(), args...);
+  return std::string(buf.data(), size);
+}
+
+}  // namespace util
